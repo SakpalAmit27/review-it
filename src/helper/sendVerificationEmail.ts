f2987c3103e0d9819b6b7ad76a34b74abf5c0312
Sakpal Amit <amitsakpal744@gmail.com>
2024-06-12 17:53:02 +0530

    implemented the api response succesfully along with the promise and react email code

--- src/helper/sendVerificationEmail.ts
+++ src/helper/sendVerificationEmail.ts
@@ -2,3 +2,33 @@
 
 
 import {resend} from '@/lib/resend'
+// importing our demo email // 
+
+import VerificationEmail from '../../emails/VerificationEmail'
+import { ApiResponse } from '@/types/ApiResponse'
+
+export async function sendVerificationEmail(
+    // below data we will take for user Verificaton /// 
+
+    
+    email:string,
+    username:string,
+    verifyCode:string,
+):Promise<ApiResponse>{
+    try{
+        // source of code is from reat email // 
+        await resend.emails.send({
+            from: 'onboarding@resend.dev',
+            to: email,
+            subject: 'review it | verification code',
+            react: VerificationEmail({username,otp:verifyCode}) // the demo email // and pass the prop method .. 
+          });
+        return {success:true , message:"verification email sent successfully"}
+
+    }catch(emailError){
+        console.log("Error sending verification email ",emailError)
+        // to shut the Promise
+        return {success:false , message:"Failed to send verification email"};
+        }
+    }
+   
