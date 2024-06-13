9acfeb8b643bb3d235452b9be7b52583e564258e
Sakpal Amit <amitsakpal744@gmail.com>
2024-06-13 12:53:45 +0530

    initial route api commit , added db connection along with registering validation to check before Api execution

--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -6,3 +6,30 @@ import UserModel from "@/model/User";
 // import bcrypt // 
 
 import bcrypt from 'bcryptjs'
+
+// importing sendVerificationemail // 
+
+import { sendVerificationEmail } from "@/helper/sendVerificationEmail";
+
+
+
+export async function POST(request: Request){
+    // lets connect the db first
+    await dbConnect()
+
+    try{
+
+    }catch(error){
+        /// console will show in terminal // 
+        console.error("Error registering the user",error)
+
+        // this return will be shown in frontend
+        return Response.json(
+            {
+                success:false,
+                message:"Error registering the user"
+            }
+        )
+    }
+
+}
